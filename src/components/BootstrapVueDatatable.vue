6ac849821dca3c3e252225948e46d396f31cbd4e
AzureAD\DragosDuma <dragos.duma@gmail.com>
2021-09-14 14:29:45 +0300

    Add search option

--- src/components/BootstrapVueDatatable.vue
+++ src/components/BootstrapVueDatatable.vue
@@ -1,10 +1,25 @@
 <template>
   <div class="container pt-3 pb-5">
     <h2>BootstrapVue Datatable</h2>
+    <b-row>
+      <b-col md="3">
+        <b-form-input v-model="filter" type="search" placeholder="Search"></b-form-input>
+      </b-col>
+    </b-row>
+    <b-row>
+      <b-col> 
+        <b-table striped hover :items="posts" :filter="filter"></b-table>
+      </b-col>
+    </b-row>
+   
   </div>
 </template>
 
 <script>
 export default {
+  props: ["posts"],
+  data: () => ({
+    filter: ""
+  })
 };
 </script>

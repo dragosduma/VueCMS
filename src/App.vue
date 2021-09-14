61c0294a3f2da3607123026ce8efa35d177be2f5
AzureAD\DragosDuma <dragos.duma@gmail.com>
2021-09-14 14:53:14 +0300

    Create vuetify table

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,25 @@
 <template>
   <div>
-    <bootstrap-vue-datatable :posts="posts"></bootstrap-vue-datatable>
     <vuetify-datatable :posts="posts"></vuetify-datatable>
   </div>
 </template>
 
 <script>
-import BootstrapVueDatatable from "./components/BootstrapVueDatatable";
 import VuetifyDatatable from "./components/VuetifyDatatable";
 export default {
   name: "App",
   components: {
-    BootstrapVueDatatable,
     VuetifyDatatable,
   },
 
   data: () => ({
     posts: [
       {
-        userId: 1,
-        id: 1,
-        title:
-          "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
-      },
-      {
-        userId: 1,
-        id: 2,
-        title: "qui est esse",
-      },
-      {
-        userId: 1,
-        id: 3,
-        title: "ea molestias quasi exercitationem repellat qui ipsa sit aut",
-      },
-      {
-        userId: 1,
-        id: 4,
-        title: "eum et est occaecati",
+        lastName:"Duma",
+        firstName:"Dragos",
+        email:"dragos.duma@gmail.com",
+        sex:"male",
+        birthday:"2000-21-02"
       },
     ],
   }),
--- src/components/VuetifyDatatable.vue
+++ src/components/VuetifyDatatable.vue
@@ -2,7 +2,33 @@
   <v-app>
     <v-main>
       <v-container>
-        <h2>Vuetify Datatable</h2>
+        <h2>Content Management System</h2>
+        <v-row>
+          <v-col md="3">
+            <v-text-field
+              v-model="search"
+              append-icon="mdi-magnify"
+              label="Search"
+              single-line
+              hide-details
+            ></v-text-field>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col>
+            <v-data-table
+              :headers="headers"
+              :items="posts"
+              :items-per-page="5"
+              :search="search"
+              class="elevation-1"
+            >
+              <template v-slot:item.actions="{ item }">
+                <v-icon small @click="deleteItem(item)">mdi-delete</v-icon>
+              </template>
+            </v-data-table>
+          </v-col>
+        </v-row>
       </v-container>
     </v-main>
   </v-app>
@@ -10,5 +36,23 @@
 
 <script>
 export default {
+  props: ["posts"],
+  data: () => ({
+    search: "",
+    headers: [
+      { text: "First Name", value: "firstName" },
+      { text: "Last Name", value: "lastName" },
+      { text: "Email", value: "email"},
+      { text: "Sex", value: "sex"},
+      { text: "Birthday", value: "birthday"},
+      { text: "Actions", value: "actions", sortable: false},
+    ],
+  }),
+  methods: {
+    deleteItem(item) {
+      const index = this.posts.indexOf((x) => x.id === item.id);
+      this.posts.splice(index, 1);
+    },
+  },
 };
 </script>
